a41fc20355075088f1ad4f66eab719d6de1325c0
Matt Piper <oepiper@gmail.com>
2023-09-23 21:23:18 -0400

    refactored search

--- app/api/search_route.py
+++ app/api/search_route.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, redirect
 from app.models import User, TextPost
 
 search_bp = Blueprint('search', __name__)
@@ -8,9 +8,9 @@ def search_user_posts(user):
     searched_user = User.query.filter(User.username.like(f"%{user}%")).first()
     if searched_user is None:
         return "User could not be found"
-    user_posts = TextPost.query.filter(TextPost.user_id == searched_user.id).all()
-    post_lists = []
-    for post in user_posts:
-        post_dict = post.to_dict()
-        post_lists.append(post_dict)
-    return post_lists
+    # user_posts = TextPost.query.filter(TextPost.user_id == searched_user.id).all()
+    # post_lists = []
+    # for post in user_posts:
+    #     post_dict = post.to_dict()
+    #     post_lists.append(post_dict)
+    return redirect(f'/api/text_posts/user_posts/{searched_user.id}')
