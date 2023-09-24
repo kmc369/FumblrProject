bb2e6324f860a67977c4935aaa003c61312d644f
wenyulll <wenyu414@gmail.com>
2023-09-24 12:06:38 -0700

    likes store and component added

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from "./session"
 import noteReducer from "./note"
+import likeReducer from './like';
 
 const rootReducer = combineReducers({
-  session:sessionReducer,
-  note:noteReducer,
+  session: sessionReducer,
+  note: noteReducer,
+  like: likeReducer,
 });
 
 
--- react-app/src/store/note.js
+++ react-app/src/store/note.js
@@ -4,24 +4,24 @@ const EDIT_COMMENT = "/edit/comment"
 
 export const CreateNote = (note) => {
     return {
-        type:CREATE_NOTE,
-        payload:note
+        type: CREATE_NOTE,
+        payload: note
     }
 }
 
 export const GetPostComments = (data) => {
     return {
         type: GET_NOTE_OF_POST,
-        payload:data
+        payload: data
 
     }
 
 }
 
-export const EditComments = (data,comment_id) => {
+export const EditComments = (data, comment_id) => {
     return {
-        type: EDIT_COMMENT ,
-        payload:{
+        type: EDIT_COMMENT,
+        payload: {
             data,
             comment_id
 
@@ -31,87 +31,82 @@ export const EditComments = (data,comment_id) => {
 
 }
 
-
-
-
-
-
-export const createNoteThunk =(note) => async (dispatch)=>{
+export const createNoteThunk = (note) => async (dispatch) => {
     // const  {content,user_id,post_id} = note
     const { content, user_id, post_id } = note
-    const response = await fetch(`/api/post/${post_id}/notes`,{
-        method:"POST",
-        headers:{
+    const response = await fetch(`/api/post/${post_id}/notes`, {
+        method: "POST",
+        headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({ content, user_id, post_id }),
     })
 
-    if(response.ok){
+    if (response.ok) {
         const data = await response.json()
         dispatch(CreateNote(data))
         return data
     }
-    else{
+    else {
         return "Not quite"
 
     }
 }
 
-export const getCommentsOfPostThunk = (current_post_id) => async (dispatch) =>{
-  
-    const response = await fetch(`/api/post/${current_post_id}/notes/get`,{
-        method:"GET"
+export const getCommentsOfPostThunk = (current_post_id) => async (dispatch) => {
+
+    const response = await fetch(`/api/post/${current_post_id}/notes/get`, {
+        method: "GET"
     })
-    if (response.ok){
+    if (response.ok) {
         const data = await response.json()
         dispatch(GetPostComments(data))
         return data
     }
-    else{
+    else {
         return "NO"
     }
 }
 
-export const EditCommentThunk = (data,current_note_id) => async (dispatch) =>{
-    console.log("The data from the thunk",data)
-    const response = await fetch(`/api/notes/${current_note_id}`,{
-        method:"PUT",
-        headers:{
+export const EditCommentThunk = (data, current_note_id) => async (dispatch) => {
+    console.log("The data from the thunk", data)
+    const response = await fetch(`/api/notes/${current_note_id}`, {
+        method: "PUT",
+        headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data)
 
     })
-    if (response.ok){
+    if (response.ok) {
         const data = await response.json()
-        dispatch(EditComments(data,current_note_id))
+        dispatch(EditComments(data, current_note_id))
         return data
     }
-    else{
+    else {
         return "NO"
     }
 }
 
 
-const initialState = {allPost:{}, singlePost:{}}
-export default function noteReducer(state=initialState,action){
+const initialState = { allPost: {}, singlePost: {} }
+export default function noteReducer(state = initialState, action) {
 
-    switch(action.type){
-        case CREATE_NOTE:{
-            const newState = { ...state, singlePost: { ...state.singlePost } }; 
+    switch (action.type) {
+        case CREATE_NOTE: {
+            const newState = { ...state, singlePost: { ...state.singlePost } };
             newState.singlePost[action.payload.post_id] = action.payload;
             return newState
         }
-        case GET_NOTE_OF_POST :{
-              const newState = {...state,singlePost:{...state.singlePost}}
-              newState.singlePost.comment = action.payload
+        case GET_NOTE_OF_POST: {
+            const newState = { ...state, singlePost: { ...state.singlePost } }
+            newState.singlePost.comment = action.payload
             return newState
         }
-        case EDIT_COMMENT:{
-            const newState = {...state,singlePost:{...state.singlePost}}
+        case EDIT_COMMENT: {
+            const newState = { ...state, singlePost: { ...state.singlePost } }
             newState.singlePost.comment[action.payload.comment_id] = action.payload.data
-         
+
             return newState
         }
         default:
