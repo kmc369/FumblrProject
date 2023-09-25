e463fdbcac2d64cd6585d8b51bf35399e580461f
kmc369 <keishacoleman710@gmail.com>
2023-09-25 17:48:32 -0400

    Model working

--- react-app/src/components/DeleteNote/index.js
+++ react-app/src/components/DeleteNote/index.js
@@ -3,11 +3,21 @@ import React, { useState ,useEffect} from "react";
 import {useParams } from 'react-router-dom';
 import { useDispatch,useSelector } from "react-redux";
 import * as NoteActions from '../../store/note'
+import OpenModalButton from '../OpenModalButton'
+import DeleteNotePopUp from '../DeletePopUpNote'
 
-function DeleteNote(comment) {
 
+
+
+function DeleteNote({comment }) {
+// console.log("the function from delete is ", openDeleteModal)
 const DropdownItems  =["Close","Delete","Edit"];
 const [open,setOpen] = useState(false)
+const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
+
+
+
 return (
     <>
     
@@ -21,6 +31,7 @@ return (
 
                 <ul>
                     {DropdownItems.map((item,index)=>(
+                      
                        <DropdownItem text={item} key={index} />
                     
                     ))}
@@ -37,15 +48,37 @@ return (
 
 }
 
-function DropdownItem({text}){
-
-    return (
-    <li className="dropdownItem" id={`${text}`}>
-        {text}
-      </li>
+export function DropdownItem({ text}) {
+    const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
    
-    )
-}
+  
+    return (
+        <li className="dropdownItem" id={`${text}`}>
+            {/* {text} */}
+       
+            <OpenModalButton
+              
+                    modalComponent={<DeleteNotePopUp/>}
+                    buttonText={text}
+                    
+            />
+       
+        </li>
+   
+        //   <li className="dropdownItem" id={`${text}`} onClick={()=>{
+        //     <OpenModalButton modalComponent={<deleteNotePopUp/>} />
+        
+          
+        //   }}>
+        //     {text}
+        //   </li>
+      
+    );
+  }
+  
+
+
+
 
 
 export default DeleteNote
