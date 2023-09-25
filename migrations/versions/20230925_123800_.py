fd9d9a515a88ef6e527997234a4aed684217ead7
kmc369 <keishacoleman710@gmail.com>
2023-09-25 12:40:36 -0400

    Instance ignored

--- migrations/versions/20230925_123800_.py
+++ migrations/versions/20230925_123800_.py
@@ -1,16 +1,18 @@
-"""create packages table
+"""empty message
 
-Revision ID: afed0c83f319
+Revision ID: 52ff45b20b45
 Revises: 
-Create Date: 2023-09-23 10:18:13.854681
+Create Date: 2023-09-25 12:38:00.729023
 
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = 'afed0c83f319'
+revision = '52ff45b20b45'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -56,6 +58,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
