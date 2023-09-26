377aa7b7a3fe32f58fdefd4716ab5725535c1e5a
wenyulll <wenyu414@gmail.com>
2023-09-25 21:46:09 -0700

    fix conflict

--- react-app/src/components/Navigation/accountDropDown.js
+++ react-app/src/components/Navigation/accountDropDown.js
@@ -1,51 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchLikesThunk } from '../../store/like';
+// import React, { useState, useEffect } from 'react';
+// import { useHistory } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { fetchLikesThunk } from '../../store/like';
 
-const AccountDropdown = ({ post_id, user_id, usernames }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const isLiked = useSelector(state => state.like.likes.user_liked);
-    console.log("im isssslaik", isLiked)
+// const AccountDropdown = ({ post_id, user_id, usernames }) => {
+//     const [isOpen, setIsOpen] = useState(false);
+//     const history = useHistory();
+//     const dispatch = useDispatch();
+//     const isLiked = useSelector(state => state.like.likes.user_liked);
+//     // console.log("im isssslaik", isLiked)
 
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
+//     const toggleMenu = () => {
+//         setIsOpen(!isOpen);
+//     };
 
-    useEffect(() => {
-        dispatch(fetchLikesThunk());
-    }, [dispatch]);
+//     useEffect(() => {
+//         dispatch(fetchLikesThunk());
+//     }, [dispatch]);
 
-    const handleLikesClick = async e => {
-        e.preventDefault();
-        history.push('/liked-posts');
-    };
+//     const handleLikesClick = async e => {
+//         e.preventDefault();
+//         history.push('/liked-posts');
+//     };
 
-    const handleLogout = () => {
+//     const handleLogout = () => {
 
-        history.push('/');
-    };
+//         history.push('/');
+//     };
 
-    return (
-        <div>
-            <button onClick={toggleMenu}>
-                Account ▼
-            </button>
-            {isOpen && (
-                <ul className="dropdown-menu">
-                    <span onClick={handleLikesClick}>Likes</span>
-                    {/* <span className="like-count">{likedPostsCount}</span> */}
-                    <li>Following</li>
-                    {/* <li>Live Stream Credits</li> */}
-                    <li onClick={handleLogout}>Logout</li>
-                </ul>
-            )}
+//     return (
+//         <div>
+//             <button onClick={toggleMenu}>
+//                 Account ▼
+//             </button>
+//             {isOpen && (
+//                 <ul className="dropdown-menu">
+//                     <span onClick={handleLikesClick}>Likes</span>
+//                     {/* <span className="like-count">{likedPostsCount}</span> */}
+//                     <li>Following</li>
+//                     {/* <li>Live Stream Credits</li> */}
+//                     <li onClick={handleLogout}>Logout</li>
+//                 </ul>
+//             )}
 
-        </div>
-    );
-};
+//         </div>
+//     );
+// };
 
-export default AccountDropdown;
+// export default AccountDropdown;
 
