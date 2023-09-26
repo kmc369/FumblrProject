c9dac5d816d84c38b5ede3ecb07060634d0f8ce9
Maria <femaleintech23@gmail.com>
2023-09-25 23:59:45 -0700

    changes on new post submission form

--- react-app/src/components/NewPost/index.js
+++ react-app/src/components/NewPost/index.js
@@ -11,7 +11,6 @@ const NewPost = ({ type }) => {
     const [postTitle, setPostTitle] = useState("")
     const [secondContent, setSecondContent] = useState("")
     const [postType, setPostType] = useState(type)
-    console.log(postType)
     const [userId, setUserId] = useState(1)
     const [errors, setErrors] = useState({})
 
