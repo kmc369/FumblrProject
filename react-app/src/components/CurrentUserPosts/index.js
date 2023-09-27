dcb0577da7e9e3fe110d86be3a83bf617460d577
wenyulll <wenyu414@gmail.com>
2023-09-27 16:05:27 -0700

    integrate current user post

--- react-app/src/components/CurrentUserPosts/index.js
+++ react-app/src/components/CurrentUserPosts/index.js
@@ -1,22 +1,22 @@
 import './CurrentUserPosts.css';
-import  { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { NavLink, useParams, useHistory } from 'react-router-dom';
 import { loadUserPostsThunk } from '../../store/post';
-
+import PostTile from '../PostTile';
 
 const CurrentUserPosts = () => {
-    const {userId} = useParams()
+    const { userId } = useParams()
     const userPosts = []
     const userPostData = useSelector(state => state.post.allPosts);
     const dispatch = useDispatch()
     const history = useHistory()
     Object.values(userPostData)?.map(post => userPosts.push(post))
-    
+
     useEffect(() => {
         dispatch(loadUserPostsThunk(userId))
     }, [dispatch, userId])
-    
+
     const handlePostClick = (postId) => {
         history.push(`/post/${postId}`);
     };
@@ -24,21 +24,22 @@ const CurrentUserPosts = () => {
     return (
         // <h1>Current User Posts Component!!</h1>
         <>
-        <div className='all-posts-container'>
-            {userPosts && userPosts.map(post => (
-                <div className='post' key={post.id} onClick={() => handlePostClick(post.id)}>
-                    <div className='user-username'>
-                        {post.user.username}
-                    </div>
-                    <div className='post-title'>
-                        {post.title}
-                    </div>
-                    <div className='post-textContent'>
-                        {post.text_content}
-                    </div>
-                </div>
-            ))}
-        </div>
+            <div className='all-posts-container'>
+                {userPosts && userPosts.map(post => (
+                    // <div className='post' key={post.id} onClick={() => handlePostClick(post.id)}>
+                    //     <div className='user-username'>
+                    //         {post.user.username}
+                    //     </div>
+                    //     <div className='post-title'>
+                    //         {post.title}
+                    //     </div>
+                    //     <div className='post-textContent'>
+                    //         {post.text_content}
+                    //     </div>
+                    // </div>
+                    <PostTile post={post} />
+                ))}
+            </div>
         </>
     )
 
